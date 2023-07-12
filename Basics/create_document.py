a9937893e5df1c8ac788564b0b6605105f6171ef
Gracjan Sadowicz <gracjan@ravendb.net>
2023-07-12 14:33:03 +0200

    RDoc-2482 Fix create document, document store holder enhancements

--- Basics/create_document.py
+++ Basics/create_document.py
@@ -1,4 +1,4 @@
-import database
+from database import DocumentStoreHolder
 from models import Company, Contact
 
 
@@ -20,10 +20,8 @@ def run(run_params: RunParams) -> str:
     # region Step_1
     new_company = Company(Name=company_name, Phone=company_phone, Contact=Contact(contact_name, contact_title))
     # endregion
-    urls = ["http://127.0.0.1:8080"]
-    store = database.DocumentStoreHolder.document_store(urls)
 
-    with store.open_session(database="Demo") as session:
+    with DocumentStoreHolder.document_store().open_session() as session:
         # region Step_2
         session.store(new_company)
         # endregion

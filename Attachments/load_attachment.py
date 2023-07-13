8a3351989bb61262dc3021415bdbdc402d6acdc7
Gracjan Sadowicz <gracjan@ravendb.net>
2023-07-13 12:54:07 +0200

    RDoc-2482 Attachments > Load Attachment

---
+++ Attachments/load_attachment.py
@@ -0,0 +1,39 @@
+# region Usings
+from copy import deepcopy
+
+# endregion
+from demo_example import RunParamsBase, DemoExample
+
+
+class RunParams(RunParamsBase):
+    def __init__(self, document_id: str, attachment_name: str):
+        self.document_id = document_id
+        self.attachment_name = attachment_name
+
+
+class LoadAttachment(DemoExample):
+    _DEFAULT_DOCUMENT_ID = "categories/1-A"
+
+    def run(self, run_params: RunParams) -> None:
+        document_id = run_params.document_id or self._DEFAULT_DOCUMENT_ID
+        attachment_name = run_params.attachment_name
+
+        # region Demo
+        with self.document_store_holder.store().open_session() as session:
+            # region Step_1
+            attachment_exists = session.advanced.attachments.exists(document_id, attachment_name)
+            # endregion
+
+            if attachment_exists:
+                # region Step_2
+                with session.advanced.attachments.get(document_id, attachment_name) as attachment_result:
+                    # endregion
+                    # region Step_3
+                    content_type = attachment_result.details.content_type
+                    hash = attachment_result.details.hash
+                    size = attachment_result.details.size
+                    # endregion
+
+                    # region Step_4
+                    attachment_data = deepcopy(attachment_result.data)
+                    # endregion

af0e82a96a7961952d3d94dc950d3ce8e495c768
Gracjan Sadowicz <gracjan@ravendb.net>
2023-07-13 14:21:40 +0200

    RDoc-2482 Queries > Projecting Individual Fields

---
+++ Queries/projecting_individual_fields.py
@@ -0,0 +1,35 @@
+from typing import List
+
+from ravendb import QueryData
+
+from demo_example import Example
+from models import Company
+
+
+class ProjectingIndividualFields(Example):
+    # region Demo
+    class CompanyDetails:
+        def __init__(self, company_name: str = None, city: str = None, country: str = None):
+            self.company_name = company_name
+            self.city = city
+            self.country = country
+
+    # endregion
+
+    def run(self, run_params=None) -> List[CompanyDetails]:
+        # region Demo
+        with self.document_store_holder.store().open_session() as session:
+            # region Step_1
+            projected_query = session.query(object_type=Company)
+            # endregion
+            # region Step_2
+            projected_query = projected_query.select_fields_query_data(
+                QueryData(["Name", "Address.City", "Address.Country"], ["company_name", "city", "country"])
+            )
+            # endregion
+
+            # region Step_3
+            projected_results = list(projected_query)
+            # endregion
+        # endregion
+        return projected_results

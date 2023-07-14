54bc67e465feafd4c490046380f52a93a60c458a
Gracjan Sadowicz <gracjan@ravendb.net>
2023-07-14 13:12:57 +0200

    RDoc-2482 Auto Indexes > Auto Map Index 1

---
+++ Auto Indexes/auto_map_index_1.py
@@ -0,0 +1,27 @@
+from demo_example import Example, RunParamsBase
+from models import Employee
+
+
+class RunParams(RunParamsBase):
+    def __init__(self, first_name: str):
+        self.first_name = first_name
+
+
+class AutoMapIndex1(Example):
+    def run(self, run_params: RunParams):
+        first_name = run_params.first_name
+
+        # region Demo
+
+        with self.document_store_holder.store().open_session() as session:
+            # region Step_1
+            find_employee_query = session.query(object_type=Employee).where_equals("FirstName", first_name)
+            # endregion
+
+            # region Step_2
+            employee_result = find_employee_query.first()
+            # endregion
+
+        # endregion
+
+        return employee_result
